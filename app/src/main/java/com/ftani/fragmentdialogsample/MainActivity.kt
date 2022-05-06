fb9bb79f59eb84a3cfb35334728c1261fae2fc85
tanifumiya <fumiyadeveloper@gmail.com>
2022-05-06 21:59:33 +0900

    Activity でダイアログのクリックイベントを取得できるように修正

--- app/src/main/java/com/ftani/fragmentdialogsample/MainActivity.kt
+++ app/src/main/java/com/ftani/fragmentdialogsample/MainActivity.kt
@@ -3,9 +3,15 @@ package com.ftani.fragmentdialogsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.ftani.fragmentdialogsample.dialog.Event
 import com.ftani.fragmentdialogsample.dialog.SampleDialogFragment
+import com.ftani.fragmentdialogsample.dialog.SampleDialogViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val dialogViewModel: SampleDialogViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +24,19 @@ class MainActivity : AppCompatActivity() {
                 .build()
                 .show(supportFragmentManager, SAMPLE_DIALOG_TAG)
         }
+
+        dialogViewModel.state.observe(this) { state ->
+            state.events.forEach { event ->
+                when(event) {
+                    Event.POSITIVE -> showToast("PositiveAction!")
+                    Event.NEGATIVE -> showToast("NegativeAction!")
+                }
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     companion object {
